563b41b08272db7e12540e7d0a44602177dcacb5
RepositorioElite <repositorio.elite@sqasa.co>
2022-10-05 17:19:39 -0500

    Agrego Codigos Gradle
    Gradle + Serenity Screenplay

---
+++ EliteScreenplay/src/main/java/UserInterface/CambioVentana.java
@@ -0,0 +1,12 @@
+package UserInterface;
+
+//import net.serenitybdd.core.annotations.findby.Findby;
+
+public class CambioVentana {
+
+
+
+  //  driver.switchTo().parentFrame().findElement(By.Localizador("NOMBRE DEL ELEMENTO A BUSCAR"));
+
+// driver.switchTo().frame("name ó id del elemento");
+}
